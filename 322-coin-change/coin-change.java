81dea5287803c9ebf4a2d1d9e24754df610672cc
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-11 15:57:13 +0530

    Time: 15 ms (78.38%) | Memory: 44 MB (83.58%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,7 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] amt = new int[amount+1];
-        Arrays.fill(amt,amount+1 );
+        Arrays.fill(amt,amount+1);
         amt[0]=0;
         for(int i =1;i<=amount;i++){
             for(int j =0;j<coins.length;j++){
@@ -10,10 +10,9 @@ class Solution {
                 }
             }
         }
-        if(amt[amount] < amount+1){
+        if(amt[amount]<amount+1){
             return amt[amount];
         }
         return -1;
-        
     }
 }
