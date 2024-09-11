1ce992d4fd40ee5ff7a7cb3bb149e184bc3d05ea
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-11 22:25:46 +0530

    Time: 20 ms (75.73%) | Memory: 50.7 MB (60.37%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,18 +1,19 @@
-class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int [][] matrix = new int[text1.length()+1][text2.length()+1];
-        for(int j = text2.length()-1;j>=0;j--){
-            for(int i = text1.length()-1;i>=0;i--){
-                if(text1.charAt(i)==text2.charAt(j)){
-                    matrix[i][j]= 1+matrix[i+1][j+1];
-                }
-                else{
-                    matrix[i][j]=Math.max(matrix[i+1][j],matrix[i][j+1]);
-                }
+public class Solution {  
+    public int longestCommonSubsequence(String text1, String text2) {  
+        // Initialize matrix with proper dimensions  
+        int[][] matrix = new int[text1.length() + 1][text2.length() + 1];  
 
-            }
-        }
-        return matrix[0][0];
+        // Build the LCS matrix  
+        for (int j = text2.length() - 1; j >= 0; j--) {  
+            for (int i = text1.length() - 1; i >= 0; i--) {  
+                if (text1.charAt(i) == text2.charAt(j)) {  
+                    matrix[i][j] = 1 + matrix[i + 1][j + 1]; // Use j+1 to match text2  
+                } else {  
+                    matrix[i][j] = Math.max(matrix[i + 1][j], matrix[i][j + 1]);  
+                }  
+            }  
+        }  
         
-    }
+        return matrix[0][0]; // Return value for the whole strings  
+    }  
 }
