f3711910060451fee4de153c2e7e9836da14d285
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-12 21:10:24 +0530

    Time: 2 ms (20.72%) | Memory: 41.6 MB (64.17%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -3,21 +3,20 @@ class Solution {
         if(s.charAt(0)=='0'){
             return 0;
         }
-        int one=1;
+        int one =1;
         int two=1;
         for(int i =1;i<s.length();i++){
-            int cur =0;
+            int current=0;
             if(s.charAt(i)!='0'){
-                cur=one;
+                current=one;
             }
-            int val = Integer.parseInt(s.substring(i-1,i+1));
-            if(val >=10 && val <=26){
-                cur=cur+two;
-                
+            int value=Integer.parseInt(s.substring(i-1,i+1));
+            if(value>=10 && value <=26){
+                current=current+two;
             }
             two=one;
-            one=cur;
-            
-        }return one;
+            one=current;
+        }
+        return one;
     }
 }
