1e320be5cb216555d9483ec052a2f89ab2343798
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-12 17:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (63.30%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,9 +3,9 @@ class Solution {
         int rob1=0;
         int rob2=0;
         int max=0;
-        for(int n : nums){
-            max=Math.max(rob1+n,rob2);
-            rob1= rob2;
+        for(int i =0;i<nums.length;i++){
+            max=Math.max(nums[i]+rob1,rob2);
+            rob1=rob2;
             rob2=max;
         }
         return max;
