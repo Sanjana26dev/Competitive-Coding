f95806d4e0567c3afcfd6f7209e2bef913db86e2
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-14 21:30:12 +0530

    Time: 24 ms (98.06%) | Memory: 50.9 MB (97.51%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
         for(int i =0;i<nums.length && nums[i]<=0;i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-                twosum2(nums,i,result);
+            if(i==0||nums[i]!=nums[i-1]){
+                twosum(nums,i,result);
             }
-            
         }
         return result;
-
         
     }
-    public void twosum2(int[] nums,int i ,List<List<Integer>> result){
-        int left =  i+1;
-        int right = nums.length-1;
+    public void twosum(int[] nums,int i ,List<List<Integer>> result)
+    {
+        int left=i+1;
+        int right=nums.length-1;
         while(left<right){
             int sum = nums[i]+nums[left]+nums[right];
             if(sum<0){
@@ -25,7 +24,7 @@ class Solution {
             }
             else{
                 result.add(Arrays.asList(nums[i],nums[left++],nums[right--]));
-                while(left < right && nums[left]==nums[left-1]){
+                while(left<right && nums[left]==nums[left-1]){
                     ++left;
                 }
             }
