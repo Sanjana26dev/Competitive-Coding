721235c47760201d1d313bdf05ead96c20aeb076
Sanjana Gangopadhyay <91048643+Sanjana26dev@users.noreply.github.com>
2024-09-15 12:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (68.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
-        int one =1;
-        int two =2;
+        if(n<=2){
+            return n;
+        }
+        int one=1;
+        int two=2;
         for(int i =3;i<=n;i++){
-            int total = one + two;
+            int total=one+two;
             one=two;
             two=total;
-
         }
         return two;
 
